ff662d5401d7258ddf767a448bb7f7b1facfd6ca
Kayli Nishime <kmnishime@ucdavis.edu>
2016-08-28 01:20:59 -0700

    added about me

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var app = angular.module("myportfolio", ['ui.router'])
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/home')
+
+  $stateProvider
+    .state('home', {
+        url: '/home',
+        controller: 'homeController as home',
+        templateUrl: 'js/partials/home.html'
+    })
+    .state('about', {
+        url: '/about',
+        controller: 'aboutController as about',
+        templateUrl: 'js/partials/about.html'
+    })
+  })
---
+++ js/controllers/aboutController.js
@@ -0,0 +1,9 @@
+(function(){
+
+angular.module('myportfolio')
+    .controller('aboutController', aboutController);
+
+function aboutController() {
+      var self = this;
+
+}
